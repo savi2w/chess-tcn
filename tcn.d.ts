1245130d7cca7a270e012d1b863ed454a1ce3753
Weslen Nascimento <weslenng@outlook.com>
2020-08-07 22:35:30 -0300

    Update .d.ts

--- tcn.d.ts
+++ tcn.d.ts
@@ -1,7 +1,2 @@
-type Move = {
-  from: string;
-  to: string;
-};
-
-declare function decode(e: string): Move;
-declare function encode(m: Move): string;
+export var decode: (e: string) => { from: string; to: string };
+export var encode: (m: { from: string; to: string }) => string;
